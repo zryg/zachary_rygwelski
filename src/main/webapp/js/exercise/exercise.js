7fdd1a15c85c232e48ee0b7871d62b18a5c1bb00
zryg <zryg@umich.edu>
2017-01-17 21:13:51 -0600

    testing

--- src/main/webapp/js/exercise/exercise.js
+++ src/main/webapp/js/exercise/exercise.js
@@ -1,15 +1,15 @@
 $(document).foundation()
 $( document ).ready(function() {
     console.log("ok");
-    $('.js-addSet').click(function() {
-        console.log("clicked");
-        $.ajax({
-            url: '/exercises/set',
-            type: 'POST',
-            data: $('#myform').serializeArray()
-            success: function() {
-                alert('PUT completed');
-            }
-        });
-   });
+//    $('.js-addSet').click(function() {
+//        console.log("clicked");
+//        $.ajax({
+//            url: '/exercises/set',
+//            type: 'POST',
+//            data: $('#myform').serializeArray()
+//            success: function() {
+//                alert('PUT completed');
+//            }
+//        });
+//   });
 });
